05302af9f33c2958f8c7e67e61b79a195ae009b8
Brian Crowley <crowley.brian2004@gmail.com>
2026-06-17 14:08:46 -0300

    Protección del endpoint de seller

--- app/api/seller/[id]/route.ts
+++ app/api/seller/[id]/route.ts
@@ -11,6 +11,18 @@ export async function GET(
   req: Request,
   { params }: Params
 ) {
+
+  const apiKey = req.headers.get("X-API-Key");
+
+  if (
+        apiKey !== process.env.INTERNAL_API_KEY
+    ) {
+        return NextResponse.json(
+        { error: "Unauthorized" },
+        { status: 401 }
+        );
+    }
+
   const { id } = await params;
 
   const seller = await prisma.seller.findUnique({
