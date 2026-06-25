983c41aa3a4118c39843770fd3f06e64f734c21f
Brian Crowley <crowley.brian2004@gmail.com>
2026-06-24 22:10:32 -0300

    Onboarding

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -2,24 +2,19 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 import OnboardingForm from "./OnboardingForm";
+import { getEffectiveUserId } from "@/lib/getEffectiveUser";
 
 export default async function OnboardingPage() {
-  const { userId, sessionClaims } = await auth();
-  
-  if (!userId) redirect("/sign-in");
+  const effectiveUserId =
+    await getEffectiveUserId();
 
-  const role =
-    (sessionClaims?.metadata as any)?.role ??
-    (sessionClaims?.publicMetadata as any)?.role ??
-    null;
-
-  if (role === "admin") {
-    redirect("/dashboard/admin");
+  if (!effectiveUserId) {
+    redirect("/sign-in");
   }
 
   const seller = await prisma.seller.findUnique({
     where: {
-      clerkUserId: userId,
+      clerkUserId: effectiveUserId,
     },
   });
 
